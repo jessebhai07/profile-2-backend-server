7d4fdea9473ef6255c624524750edd768520bda2
rimu-7 <rrimu99@gmail.com>
2025-02-19 12:05:25 +0600

    server

--- server.js
+++ server.js
@@ -218,7 +218,5 @@ app.get("/api/blogs/:blog_id", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-
-
 // Export the app (required for Vercel)
 module.exports = app;
